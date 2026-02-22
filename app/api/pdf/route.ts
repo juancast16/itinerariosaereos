0ee5c035529632ce76eda478188be2748f94e3e4
Juan Castañeda <juancastaneda93@hotmail.com>
2026-02-22 17:06:00 -0500

    Fix chromium headless typing

--- app/api/pdf/route.ts
+++ app/api/pdf/route.ts
@@ -11,10 +11,17 @@ export async function POST(req: Request) {
   const itinerary = await req.json()
 
   const browser = await puppeteer.launch({
-    args: chromium.args,
-    executablePath: await chromium.executablePath(),
-    headless: true
-  })
+  args: [
+    ...chromium.args,
+    '--no-sandbox',
+    '--disable-setuid-sandbox',
+    '--disable-dev-shm-usage',
+    '--single-process',
+    '--no-zygote'
+  ],
+  executablePath: await chromium.executablePath(),
+  headless: true
+})
 
   const page = await browser.newPage()
 
